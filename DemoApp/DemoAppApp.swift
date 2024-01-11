799f6e5fa8d91ec120fb5d24a1afac9d287a37cf
Nishanth <nishanth.m@pearson.com>
2024-01-11 16:26:35 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by M_AMBIN05470 on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
